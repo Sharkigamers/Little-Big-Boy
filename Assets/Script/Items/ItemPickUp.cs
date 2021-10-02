4a08756ddcf8287376cda2a2c5b634f31c2fdb1b
Danny <gabriel.danjon@epitech.eu>
2021-10-03 00:23:12 +0900

    [ADD]: Inventory item adder

---
+++ Assets/Script/Items/ItemPickUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public Item item;
+
+    void PickUp() {
+        bool wasPickedUp = Inventory.instance.Add(item);
+
+        if (wasPickedUp)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("Collectible"))
+            hit.gameObject.SendMessage("PickUp");
+    }
+}
