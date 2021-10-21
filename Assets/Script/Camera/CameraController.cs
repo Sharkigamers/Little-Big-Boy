56fe3b4b7a3c8d644c5021761f764f4e376e7630
Danny <gabriel.danjon@epitech.eu>
2021-10-21 20:21:54 +0900

    [ADD]: level design

--- Assets/Script/Camera/CameraController.cs
+++ Assets/Script/Camera/CameraController.cs
@@ -17,7 +17,7 @@ public class CameraController : MonoBehaviour
      private float zoom;
 
     Vector3 offsetPosition = new Vector3(0f, 1.5f, -5.5f);
-    Vector3 offsetZoomPosition = new Vector3(0f, 0.25f, -1.5f);
+    Vector3 offsetZoomPosition = new Vector3(0f, 0.5f, -2f);
 
     // Start is called before the first frame update
     void Start()
