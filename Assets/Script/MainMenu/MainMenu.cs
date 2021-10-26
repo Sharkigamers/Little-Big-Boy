010c4c94b372101ebb1b4ffad8b0a3a52569d851
jsatge <jeannie.satge@epitech.eu>
2021-10-26 21:55:14 +0900

    feat: adding all menu

---
+++ Assets/Script/MainMenu/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Settings() {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Back() {
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void ReplayGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Menu() {
+        SceneManager.LoadScene(0);
+    }
+}
